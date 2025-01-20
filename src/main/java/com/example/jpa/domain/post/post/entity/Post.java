27e028a6fd7b5dc88d5bfeccf568e5682a73dd2e
asungkim <asungkim007@naver.com>
2025-01-20 10:39:48 +0900

    baseInitData

--- src/main/java/com/example/jpa/domain/post/post/entity/Post.java
+++ src/main/java/com/example/jpa/domain/post/post/entity/Post.java
@@ -1,19 +1,29 @@
 package com.example.jpa.domain.post.post.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Setter
 public class Post {
 
     @Id // PR
     @GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO-INCREMENT
-    private long id;
+    @Getter
+    private Long id; // long -> null x , LOng -> null o
     private LocalDateTime createdDate;
     private LocalDateTime modifiedDate;
     @Column(length = 100)
     private String title;
     @Column(columnDefinition = "TEXT")
     private String body;
+
+
+
+
+
+
 }
