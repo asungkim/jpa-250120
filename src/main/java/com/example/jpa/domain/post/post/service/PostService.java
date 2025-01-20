dfe613b6b50e99c8ce6f655bf3a26024875329f4
asungkim <kimasung143@gmail.com>
2025-01-20 10:39:48 +0900

    baseInitData

---
+++ src/main/java/com/example/jpa/domain/post/post/service/PostService.java
@@ -0,0 +1,31 @@
+package com.example.jpa.domain.post.post.service;
+
+import com.example.jpa.domain.post.post.entity.Post;
+import com.example.jpa.domain.post.post.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+
+    private final PostRepository postRepository;
+
+    public Post write(String title, String body) {
+
+        // 1. Post 조립
+        Post post=new Post();
+//        post.setId(1L); id는 JPA가 관리함
+        post.setCreatedDate(LocalDateTime.now());
+        post.setModifiedDate(LocalDateTime.now());
+        post.setTitle(title);
+        post.setBody(body);
+
+        // 2. repository 에 넘김
+        postRepository.save(post);
+
+        return post;
+    }
+}
