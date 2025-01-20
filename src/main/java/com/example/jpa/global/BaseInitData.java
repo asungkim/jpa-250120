a375bfe4f4d4b4418cd84334e265b8a973cabd21
asungkim <kimasung143@gmail.com>
2025-01-20 15:41:43 +0900

    comment

--- src/main/java/com/example/jpa/global/BaseInitData.java
+++ src/main/java/com/example/jpa/global/BaseInitData.java
@@ -1,5 +1,7 @@
 package com.example.jpa.global;
 
+import com.example.jpa.domain.post.comment.entity.Comment;
+import com.example.jpa.domain.post.comment.service.CommentService;
 import com.example.jpa.domain.post.post.entity.Post;
 import com.example.jpa.domain.post.post.service.PostService;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class BaseInitData {
 
     private final PostService postService;
+    private final CommentService commentService;
 
     @Bean
     @Order(1)
@@ -45,15 +48,6 @@ public class BaseInitData {
     @Bean
     @Order(2)
     public ApplicationRunner applicationRunner2() {
-//        return args -> {
-//            Post post=postService.findById(1L).get();
-//
-//            Thread.sleep(1000);
-//
-//            postService.modify(post,"new title","new content");
-//            postService.modify2(1L,"new title","new content");
-//        };
-
         return new ApplicationRunner() {
             @Override
             @Transactional
@@ -70,38 +64,15 @@ public class BaseInitData {
     public ApplicationRunner applicationRunner3() {
         return new ApplicationRunner() {
             @Override
-            @Transactional
             public void run(ApplicationArguments args) throws Exception {
-                Post p1=postService.findById(1L).get();
-                Post p2=postService.findById(2L).get();
 
-                System.out.println("==========p1 삭제==========");
-                postService.delete(p1);
-                System.out.println("==========p1 삭제 완료==========");
-                System.out.println("==========p2 삭제==========");
-                postService.delete(p2);
-                System.out.println("==========p2 삭제 완료==========");
+                Post post = postService.findById(1L).get();
 
-//                postService.deleteById(1L);
-//                postService.deleteById(2L);
-            }
-        };
-    }
+                Comment c1 = commentService.write(post.getId(), "comment 1");
+                Comment c2 = commentService.write(post.getId(), "comment 2");
+                Comment c3 = commentService.write(post.getId(), "comment 3");
 
-    @Bean
-    @Order(4)
-    public ApplicationRunner applicationRunner4() {
-        return new ApplicationRunner() {
-            @Override
-            public void run(ApplicationArguments args) throws Exception {
-                Post post=postService.findById(3L).get();
 
-                System.out.println(post.getId()+"번 포스트를 가져왔습니다.");
-
-                System.out.println("=============================.");
-
-                Post post2=postService.findById(3L).get();
-                System.out.println(post2.getId()+"번 포스트를 가져왔습니다.");
 
             }
         };
