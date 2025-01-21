612c9ae66740c9e3a7f25968fb1ac701fda8e877
asungkim <asungkim007@naver.com>
2025-01-21 10:21:40 +0900

    entity join

--- src/main/java/com/example/jpa/domain/post/comment/entity/Comment.java
+++ src/main/java/com/example/jpa/domain/post/comment/entity/Comment.java
@@ -1,5 +1,6 @@
 package com.example.jpa.domain.post.comment.entity;
 
+import com.example.jpa.domain.post.post.entity.Post;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
@@ -34,5 +35,9 @@ public class Comment {
     @Column(columnDefinition = "TEXT")
     private String body;
 
-    private Long postId;
+
+    // fetch -> 언제 post의 정보가 필요한가요? -> LAZY = 필요할때 주세요
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id") // post_id 라는 외래키 컬럼이 생성됨
+    private Post post;
 }
