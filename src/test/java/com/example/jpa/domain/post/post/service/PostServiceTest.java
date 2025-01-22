9b1f62ff48febda9ef03ee92c3093f4d2df950b6
asungkim <kimasung143@gmail.com>
2025-01-22 14:40:34 +0900

    findAll page

--- src/test/java/com/example/jpa/domain/post/post/service/PostServiceTest.java
+++ src/test/java/com/example/jpa/domain/post/post/service/PostServiceTest.java
@@ -5,6 +5,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
@@ -105,8 +109,18 @@ public class PostServiceTest {
     public void t9() {
         // select * from post like 2,2
 
+        int itemsPerPage=2;
+        int pageNumber=2;
+        pageNumber--;
+        Pageable pageable = PageRequest.of(pageNumber, itemsPerPage, Sort.by(Sort.Direction.DESC, "id"));
+        // id 5 4 | (3 2) | 1
+        Page<Post> postPage = postService.findAll(pageable);
+        List<Post> posts = postPage.getContent(); //
 
+        assertThat(posts.size()).isEqualTo(2);
 
+        assertThat(posts.get(0).getId()).isEqualTo(3);
+        assertThat(posts.get(1).getId()).isEqualTo(2);
     }
 
 }
