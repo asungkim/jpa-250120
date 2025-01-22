72fee4e59094fc6ecd30e0708cb49a7be0ca2b86
asungkim <kimasung143@gmail.com>
2025-01-22 14:27:53 +0900

    jpa naming method

--- src/main/java/com/example/jpa/domain/post/post/repository/PostRepository.java
+++ src/main/java/com/example/jpa/domain/post/post/repository/PostRepository.java
@@ -11,4 +11,10 @@ public interface PostRepository extends JpaRepository<Post,Long> {
     List<Post> findByTitle(String title);
 
     List<Post> findByTitleAndBody(String title, String content);
+
+    List<Post> findByTitleLike(String title);
+
+    List<Post> findByOrderByIdDesc();
+
+    List<Post> findTop2ByTitleOrderByIdDesc(String title);
 }
--- src/test/java/com/example/jpa/domain/post/post/service/PostServiceTest.java
+++ src/test/java/com/example/jpa/domain/post/post/service/PostServiceTest.java
@@ -26,7 +26,7 @@ public class PostServiceTest {
     @Test
     @DisplayName("글 2개 작성")
     @Transactional
-    @Rollback(value = false)
+    @Rollback(value = true)
     public void t1() {
         postService.write("title1","body1");
         postService.write("title2","body2");
@@ -37,7 +37,7 @@ public class PostServiceTest {
     @DisplayName("모든 글 조회")
     public void t2() {
         List<Post> all = postService.findAll();
-        assertThat(all.size()).isEqualTo(3);
+        assertThat(all.size()).isEqualTo(5);
 
         Post p = all.get(0);
         assertThat("title1").isEqualTo(p.getTitle());
@@ -59,9 +59,9 @@ public class PostServiceTest {
     @Test
     @DisplayName("제목으로 조회")
     public void t4() {
-        List<Post> posts=postService.findByTitle("title2");
+        List<Post> posts=postService.findByTitle("title1");
 
-        assertThat(posts.size()).isEqualTo(1);
+        assertThat(posts.size()).isEqualTo(3);
     }
 
     @Test
@@ -69,7 +69,44 @@ public class PostServiceTest {
     public void t5() {
         List<Post> posts=postService.findByTitleAndBody("title1","content1");
 
-        assertThat(posts.size()).isEqualTo(1);
+        assertThat(posts.size()).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("제목이 포함된 결과 조회")
+    public void t6() {
+        List<Post> posts=postService.findByTitleLike("title%");
+
+        assertThat(posts.size()).isEqualTo(5);
+    }
+
+    @Test
+    @DisplayName("아이디 순으로 내림차순 정렬")
+    public void t7() {
+        List<Post> posts=postService.findByOrderByIdDesc();
+        assertThat(posts.get(0).getId()).isEqualTo(5);
+
+    }
+
+    @Test
+    @DisplayName("위에서 2개만 조회")
+    public void t8() {
+        // select * from post where title=? order by id desc limit 2;
+        List<Post> posts=postService.findTop2ByTitleOrderByIdDesc("title1");
+
+        // title1 id -> 1 4 5
+        assertThat(posts.get(0).getId()).isEqualTo(5);
+        assertThat(posts.size()).isEqualTo(2);
+
+    }
+
+    @Test
+    @DisplayName("findAll(Pageable)")
+    public void t9() {
+        // select * from post like 2,2
+
+
+
     }
 
 }
