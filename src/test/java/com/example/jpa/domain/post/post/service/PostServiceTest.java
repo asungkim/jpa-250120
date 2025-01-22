bc71a14aacfae8a72d4f2d0f3dd372a0ffc1f732
asungkim <asungkim007@naver.com>
2025-01-22 12:10:10 +0900

    test active profile

--- src/test/java/com/example/jpa/domain/post/post/service/PostServiceTest.java
+++ src/test/java/com/example/jpa/domain/post/post/service/PostServiceTest.java
@@ -2,9 +2,11 @@ package com.example.jpa.domain.post.post.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class PostServiceTest {
     @Test
     void t1() {
