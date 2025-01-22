1a81a7d57694d8e5a934b0db37c8851bf9af057a
asungkim <kimasung143@gmail.com>
2025-01-22 12:48:59 +0900

    findAll, findByTitle

--- src/test/java/com/example/jpa/domain/post/post/service/PostServiceTest.java
+++ src/test/java/com/example/jpa/domain/post/post/service/PostServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.jpa.domain.post.post.service;
 
+import com.example.jpa.domain.post.post.entity.Post;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 @SpringBootTest
@@ -21,9 +27,41 @@ public class PostServiceTest {
     @DisplayName("글 2개 작성")
     @Transactional
     @Rollback(value = false)
-    void t1() {
+    public void t1() {
         postService.write("title1","body1");
         postService.write("title2","body2");
 
     }
+
+    @Test
+    @DisplayName("모든 글 조회")
+    public void t2() {
+        List<Post> all = postService.findAll();
+        assertThat(all.size()).isEqualTo(3);
+
+        Post p = all.get(0);
+        assertThat("title1").isEqualTo(p.getTitle());
+    }
+
+
+    @Test
+    @DisplayName("특정 글 조회")
+    public void t3() {
+        Optional<Post> opPost = postService.findById(1L);
+
+        if (opPost.isPresent()) {
+            assertThat(opPost.get().getTitle()).isEqualTo("title1");
+        }
+
+
+    }
+
+    @Test
+    @DisplayName("제목으로 조회")
+    public void t4() {
+        List<Post> posts=postService.findByTitle("title2");
+
+        assertThat(posts.size()).isEqualTo(1);
+    }
+
 }
