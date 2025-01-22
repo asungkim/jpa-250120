59e7a1e5945e270b95db63c5fbd14d9eb82b7ac2
asungkim <asungkim007@naver.com>
2025-01-22 12:19:15 +0900

    post write

--- src/test/java/com/example/jpa/domain/post/post/service/PostServiceTest.java
+++ src/test/java/com/example/jpa/domain/post/post/service/PostServiceTest.java
@@ -1,6 +1,8 @@
 package com.example.jpa.domain.post.post.service;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -8,8 +10,17 @@ import org.springframework.test.context.ActiveProfiles;
 @SpringBootTest
 @ActiveProfiles("test")
 public class PostServiceTest {
+
+    @Autowired
+    private PostService postService;
+
+
     @Test
+    @DisplayName("글 2개 작성")
     void t1() {
-        System.out.println("test");
+        postService.write("title1","body1");
+        postService.write("title2","body2");
+
+
     }
 }
