fc614b02c6d6bbbfaf65857ac620100c6b2d003c
asungkim <kimasung143@gmail.com>
2025-01-22 15:41:13 +0900

    member add

--- src/main/java/com/example/jpa/global/BaseInitData.java
+++ src/main/java/com/example/jpa/global/BaseInitData.java
@@ -1,5 +1,7 @@
 package com.example.jpa.global;
 
+import com.example.jpa.domain.member.entity.Member;
+import com.example.jpa.domain.member.service.MemberService;
 import com.example.jpa.domain.post.comment.entity.Comment;
 import com.example.jpa.domain.post.comment.service.CommentService;
 import com.example.jpa.domain.post.post.entity.Post;
@@ -19,6 +21,7 @@ public class BaseInitData {
 
     private final PostService postService;
     private final CommentService commentService;
+    private final MemberService memberService;
 
     @Autowired
     @Lazy
@@ -34,20 +37,31 @@ public class BaseInitData {
     }
 
     @Transactional
-    public void work2() {
-
+    public void work1() {
+        if (memberService.count() > 0) {
+            return;
+        }
+        // 회원 생성
+        memberService.join("system","1234","시스템");
+        memberService.join("admin","1234","관리자");
+        memberService.join("user1","1234","유저1");
+        memberService.join("user2","1234","유저2");
+        memberService.join("user3","1234","유저3");
     }
 
     @Transactional
-    public void work1() {
+    public void work2() {
         if (postService.count() > 0) {
             return;
         }
-        Post p1 = postService.write("title1", "content1");
-        Post p2 = postService.write("title2", "content2");
-        Post p3 = postService.write("title3", "content3");
-        Post p4 = postService.write("title1", "content1");
-        Post p5 = postService.write("title1", "content1");
+
+        Member user1=memberService.findByUsername("user1").get();
+        Member user2=memberService.findByUsername("user2").get();
+
+        Post p1 = postService.write(user1,"title1", "content1");
+        Post p2 = postService.write(user2,"title2", "content2");
+        Post p3 = postService.write(user2,"title3", "content3");
+
 
         Comment c1 = Comment.builder().body("comment1").build();
         p1.addComment(c1); // 관계의 주인이 DB 반영을 한다
